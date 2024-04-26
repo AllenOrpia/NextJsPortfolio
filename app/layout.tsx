84aeb0ecc7cd1c0d1dffa2ce29140a6d20117ebf
Allen Orpia <allenorpia17@gmail.com>
2024-04-26 15:16:37 -0700

    added dark mode component and logic

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/footer";
+import ThemeToggle from "@/components/theme-toggle";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}
+        className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
         <div className="-z-10 bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="-z-10 bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl-left-[-15rem] 2xl:left-[-5rem]"></div>
@@ -32,6 +33,7 @@ export default function RootLayout({
           <Footer />
           <Toaster position="top-right" />
         </ActiveSectionContextProvider>
+        <ThemeToggle />
       </body>
     </html>
   );
---
+++ components/theme-toggle.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import React, { useEffect, useState } from 'react';
+import {  BsSun } from 'react-icons/bs';
+import { BsMoonStars } from "react-icons/bs";
+
+type Theme = 'light' | 'dark';
+
+export default function ThemeToggle() {
+    const [theme, setTheme] = useState<Theme>('light');
+    console.log(theme)
+
+    const toggleTheme = () => {
+        if (theme === 'light') {
+            setTheme('dark');
+            window.localStorage.setItem('theme', 'dark');
+            document.documentElement.classList.add("dark")
+        } else {
+            setTheme('light');
+            window.localStorage.setItem('theme', 'light');
+            document.documentElement.classList.remove("dark")
+        }
+    };
+
+    useEffect(() => {
+        const localTheme = window.localStorage.getItem('theme') as Theme | null;
+        if (localTheme) {
+            setTheme(localTheme);
+
+            if (localTheme === 'dark') {
+                document.documentElement.classList.add("dark")
+            }
+        } else if (window.matchMedia("(prefers-color-scheme:dark)").matches){
+            setTheme('dark');
+            document.documentElement.classList.add("dark")
+        }
+    }, [theme])
+  return (
+    <button className={`fixed bottom-8 right-8 bg-white w-12 h-12 bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-125 active:scale-105 transition-all dark:bg-gray-950 dark:text-blue-300  `} onClick={toggleTheme}>
+
+        {
+            theme === 'light' ? (
+                <BsSun />
+            ) : (
+                <BsMoonStars />
+            )
+        }
+    </button>
+  )
+}
